3c427aaa9618f1a8a3ad7bdd678dfbdba4365f7a
ksabishekkumar-art <ks.abishekkumar@gmail.com>
2026-01-02 13:06:41 +0530

    Update NthLargest.java

--- src/main/java/org/example/NthLargest.java
+++ src/main/java/org/example/NthLargest.java
@@ -1,22 +1,29 @@
-import java.util.Arrays;
-
 public class NthLargest {
+
     public static void main(String[] args) {
 
         int[] arr = {10, 45, 3, 99, 23, 78};
+        int n = 2;   // 👈 change only this
 
-        int n = 2; // 👈 change this: 1 = largest, 2 = second, 3 = third
+        int result = 0;
 
-        if (n > arr.length) {
-            System.out.println("Invalid input");
-            return;
+        for (int i = 1; i <= n; i++) {
+
+            int max = Integer.MIN_VALUE;
+            int index = 0;
+
+            // find largest
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                    index = j;
+                }
+            }
+
+            result = max;                 // store result
+            arr[index] = Integer.MIN_VALUE; // remove used value
         }
 
-        // descending order sort
-        Arrays.sort(arr);
-
-        int result = arr[arr.length - n];
-
-        System.out.println(n + "th Largest Number: " + result);
+        System.out.println(n + "th Largest = " + result);
     }
 }
